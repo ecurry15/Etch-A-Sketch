3db01afbd73eaa564192275c5bdcf6966b7b6da1
Edward <Ecampbell3015@gmail.com>
2022-05-10 14:18:15 -0700

    Change square color with mouse click/drag

--- script.js
+++ script.js
@@ -4,28 +4,49 @@ const sliderOutput = document.getElementById('sliderText');
 sliderOutput.textContent = `${rangeSlider.value} x ${rangeSlider.value}`;
 
 const gridContainer = document.querySelector('#paper');
-
+let mousePressed = false;
 
 
 
 const makeRows = function (rows, cols) {
-  gridContainer.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
+//DYNAMICALLY SETS GRID TEMPLATE ---
+gridContainer.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
 gridContainer.style.gridTemplateRows = `repeat(${cols}, 1fr)`;
-let divs = gridContainer.querySelectorAll('div');
-divs.forEach((div) => div.remove());
 
+//SELECTS AND REMOVES PREVIOUS DIVS ---
+let oldDivs = gridContainer.querySelectorAll('div');
+oldDivs.forEach((div) => div.remove());
+
+//GENERATES DIVS BASED ON THE SPECIFIED ROWS/COLS ---
   for (let i = 0; i < rows * cols; i++) {
     let square = document.createElement('div');
     gridContainer.insertAdjacentElement('beforeend', square);
     gridContainer.appendChild(square).id = "grid-item";
   }
-  let squareDivs = gridContainer.querySelectorAll('div');
-  squareDivs.forEach((div) => div.addEventListener('click', function () {
-    div.style.backgroundColor = "black";
+
+//SELECTS ALL NEWLY CREATED DIVS ---
+  let newDivs = gridContainer.querySelectorAll('div');
+
+//CHECKS FOR MOUSE CLICK / UNCLICK ---
+  document.addEventListener('mousedown', function() {
+    mousePressed = true;
+  });
+
+  document.addEventListener('mouseup', function() {
+    mousePressed = false;
+  });
+
+  //IF MOUSE IS CLICKED CHANGE SQAURES TO BLUE ---
+  newDivs.forEach((div) => div.addEventListener('mousemove', function() {
+    if (mousePressed) {
+      div.style.backgroundColor = "rgb(6,57,112)";
+    }
   }))
+
 };
 
 
+//DISPLAYS RANGE SLIDER INPUT AND MAKES ROWS/COLS---
 rangeSlider.oninput = function () {
   sliderOutput.textContent = `${this.value} x ${this.value}`;
   makeRows(this.value, this.value);
