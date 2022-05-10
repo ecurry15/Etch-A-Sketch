7b6354123922f9c5a3bd09f09b19f43bfaa5178a
Edward <Ecampbell3015@gmail.com>
2022-05-10 12:37:05 -0700

    Add responsive grid template and an event listner to change squares to black on click

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 "use strict";
+const rangeSlider = document.getElementById('sliderRange');
+const sliderOutput = document.getElementById('sliderText');
+sliderOutput.textContent = `${rangeSlider.value} x ${rangeSlider.value}`;
+
+const gridContainer = document.querySelector('#paper');
+
+
+
+
+const makeRows = function (rows, cols) {
+  gridContainer.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
+gridContainer.style.gridTemplateRows = `repeat(${cols}, 1fr)`;
+let divs = gridContainer.querySelectorAll('div');
+divs.forEach((div) => div.remove());
+
+  for (let i = 0; i < rows * cols; i++) {
+    let square = document.createElement('div');
+    gridContainer.insertAdjacentElement('beforeend', square);
+    gridContainer.appendChild(square).id = "grid-item";
+  }
+  let squareDivs = gridContainer.querySelectorAll('div');
+  squareDivs.forEach((div) => div.addEventListener('click', function () {
+    div.style.backgroundColor = "black";
+  }))
+};
+
+
+rangeSlider.oninput = function () {
+  sliderOutput.textContent = `${this.value} x ${this.value}`;
+  makeRows(this.value, this.value);
+}
+
+
+
+
 
